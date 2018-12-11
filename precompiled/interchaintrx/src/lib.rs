b393add7a74c8d701a355e108f0797731a4528a0
Harish <harishbm34@gmail.com>
2018-12-11 16:38:38 +0800

    Environment variable name changed in lib.rs

--- precompiled/interchaintrx/src/lib.rs
+++ precompiled/interchaintrx/src/lib.rs
@@ -9,16 +9,14 @@ extern crate bn;
 
 use web3::futures::Future;
 use ethereum_types::{Address, Public};
-use web3::types::{H256, H520, U256, Transaction, TransactionId};
+use web3::types::{H256, H520};
 use secp256k1::{Message, RecoverableSignature, RecoveryId, Error as SecpError};
 use tiny_keccak::Keccak;
 use sputnikvm::Precompiled;
 use std::rc::Rc;
 use std::cmp::min;
 use bigint::Gas;
-use web3::Error;
 use std::env;
-
 
 
 pub static INT_CHAIN_TRX_PRECOMPILED: InterChainTrxPrecompiled = InterChainTrxPrecompiled;
@@ -26,13 +24,11 @@ pub static INT_CHAIN_TRX_PRECOMPILED: InterChainTrxPrecompiled = InterChainTrxPr
 pub struct InterChainTrxPrecompiled;
 impl Precompiled for InterChainTrxPrecompiled {
 
-    fn gas(&self, data: &[u8]) -> Gas {
-        /// TODO: Calculate the gas amount
+    fn gas(&self, _data: &[u8]) -> Gas {
         Gas::from(3000u64)
     }
 
     fn step(&self, datao: &[u8]) -> Rc<Vec<u8>> {
-        println!("Data in Precompiled contract : {:?}", datao);
         let mut data = [0u8; 128];
         let copy_bytes = min(datao.len(), 128);
         data[..copy_bytes].clone_from_slice(&datao[..copy_bytes]);
@@ -51,7 +47,6 @@ impl Precompiled for InterChainTrxPrecompiled {
 }
 
 fn inter_chain_trx(data: &[u8]) -> Result<bool, SecpError> {
-    println!("Entering into interchaintrx : {:?}", data);
     let mut tx1_from = [0u8; 20];
     for i in 0..20 {
         tx1_from[i] = data[i];
@@ -68,7 +63,7 @@ fn inter_chain_trx(data: &[u8]) -> Result<bool, SecpError> {
     }
 
     let url =  env::var("MAINNET_URL").expect("Environment variable has not set");
-    let (_eloop, transport) = web3::transports::Http::new(&url).unwrap();
+    let (_eloop, transport) = web3::transports::Http::new(&url).expect("unable to create Web3 HTTP provider");
     let web3 = web3::Web3::new(transport);
 
     let tx1_from = address_from_slice(&tx1_from);
@@ -77,40 +72,50 @@ fn inter_chain_trx(data: &[u8]) -> Result<bool, SecpError> {
     
     let transaction = match web3.eth().transaction(web3::types::TransactionId::Hash(tx1_hash)).wait().unwrap() {
         None => {
-            return Err(SecpError::InvalidMessage); //Invalid Transaction Hash
+            return Ok(false);
         }
         Some(transaction) => transaction,  
     };
-    println!("Transactions - From Address : {:#?}", transaction.from);
 
-    let tx_receipt = match web3.eth().transaction_receipt(tx1_hash).wait().unwrap() {
-        None => {
-            return Err(SecpError::InvalidMessage); //Invalid Transaction Hash
-        }
-        Some(tx_receipt) => tx_receipt,  
+    //Verify Transaction-1's "to" address is present on "Ethereum" network.   
+    let from_trx: ethereum_types::H160 = match transaction.to {
+        Some(val) => val,
+        None => return Err(SecpError::InvalidMessage),
     };
-    println!("Transaction Receipt - Transaction Hash  : {:#?}", tx_receipt.transaction_hash);
-    println!("Status of the Transaction on Ethereum : {:?}", tx_receipt.status);
 
+    let verified: bool = verify_to_address(&from_trx, &tx1_from);
 
-    let balance = match get_balance(&web3, &tx1_from) {
-        Ok(value) => value,
-        Err(_) => {
-            eprint!("Balance not found for {:?}", tx1_from);
-            std::process::exit(-1)
+    if verified == true {
+        let tx_receipt = match web3.eth().transaction_receipt(tx1_hash).wait().unwrap() {
+            None => {
+                return Err(SecpError::InvalidMessage); //Invalid Transaction Hash
+            }
+            Some(tx_receipt) => tx_receipt,  
+        };
+
+        //Verify Transaction-1's status Success/Failure
+        let status = match tx_receipt.status {
+            Some(val) => val,
+            None => return Err(SecpError::InvalidMessage),
+        };
+
+        let verify_stat: bool = verify_status(&status);
+
+        if verify_stat == true {
+            //Check whether the transaction had happened on the "Ethereum" network.
+            let eth_msg_hash = ethereum_hash(&tx1_hash);
+            match verify_address(&tx1_from, &sig_tx1_hash, &eth_msg_hash) {
+                Ok(true) => Ok(true),
+                Ok(false) => Ok(false),
+	            Err(SecpError::InvalidSignature) => Ok(false),
+	            Err(x) => Err(x.into()),
+            }
+        } else {
+            Ok(false)
         }
-    };
-    println!("Balance : {:?}", balance);
-
-    // 1: Verify the "FROM" address using TrxHash and TrxHashSignature
-    let eth_msg_hash = ethereum_hash(&tx1_hash); // hash of tx1_hash
-    match verify_address(&tx1_from, &sig_tx1_hash, &eth_msg_hash) {
-        Ok(true) => Ok(true),
-        Ok(false) => Ok(false),
-		Err(SecpError::InvalidSignature) => Ok(false),
-		Err(x) => Err(x.into()),
+    } else {
+        Ok(false)
     }
-
 }
 
 fn keccak256(input: &[u8]) -> [u8; 32] {
@@ -123,7 +128,7 @@ fn keccak256(input: &[u8]) -> [u8; 32] {
 
 pub fn verify_address(address: &[u8], sig_tx1_hash: &[u8], eth_msg_hash: &[u8]) -> Result<bool, SecpError> {
 	let public = recover(sig_tx1_hash, eth_msg_hash)?;
-	let recovered_address = public_to_address(&public); //Public is nothing but address here
+	let recovered_address = public_to_address(&public);
 	Ok(address == &recovered_address)
 }
 
@@ -163,11 +168,24 @@ pub fn tx1_signature_hash_from_slice(s: &[u8]) -> [u8;65] {
 pub fn ethereum_hash(msg: &[u8]) -> [u8; 32] {
   let mut message = format!("\x19Ethereum Signed Message:\n{}", msg.len()).into_bytes();
   message.extend_from_slice(&msg[..]);
-  keccak256(&message[..]) // message_hash is the hash of tx1_hash
+  keccak256(&message[..])
 }
 
-fn get_balance(web3: &web3::Web3<web3::transports::Http>, address: &web3::types::Address) -> Result<U256, Error> {
-    web3.eth().balance(*address, None).wait()
+pub fn verify_to_address(trx_addr: &ethereum_types::H160, from_addr: &ethereum_types::H160) -> bool {
+    if trx_addr == from_addr {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+pub fn verify_status(status: &ethereum_types::U64) -> bool {
+    let status_check = status.as_usize();   
+    if status_check == 1 {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 #[cfg(test)]
@@ -176,17 +194,12 @@ mod tests {
     use super::*;
     use self::hexutil::*;
     #[test]
-    fn test_inter_chain_trx() {
-        // result: true
-        //ADDRESS : 8be74d722deb7c2ba962be126ae9c00f121e3562
-        //TRX-HASH     : e62db8a544dfc640c63703f53b91579f7fec46f066432b56ec7daf5a0b7740bc
-        //SIGNATURE : c7acecc3e84328c32fd6ee8a5264e203d371a1bff5561176906afdcf988a32097504b088abac55575820fc1db637b138bb05de61c942847809a1ee127866dba31c
-        println!("Entering into interchaintrx");
+    fn test_inter_chain_trx_1() {
+        // Result: true (Successful Transaction)
         let address = "8be74d722deb7c2ba962be126ae9c00f121e3562";
         let message = "e62db8a544dfc640c63703f53b91579f7fec46f066432b56ec7daf5a0b7740bc";
-        let signature = "de56cd22f0f006c9cf1b7d96fdbbaa29800be86d57961b55ea42dbe66e0edb953777990c13adcc5de39cf670a46f18148d7290a110da92ea76ac405f23428ff41b";
+        let signature = "c7acecc3e84328c32fd6ee8a5264e203d371a1bff5561176906afdcf988a32097504b088abac55575820fc1db637b138bb05de61c942847809a1ee127866dba31c";
         let together = format!("{}{}{}", address, message, signature);
-        println!("Final Input String : {:?}", together);
         let input1 = read_hex(&together).unwrap();
         let ret = inter_chain_trx(&input1);
         assert_eq!(ret.ok(), Some(true));
@@ -194,20 +207,49 @@ mod tests {
 
     #[test]
     fn test_inter_chain_trx_2() {
-        // result: false
-        //ADDRESS   : 8be74d722deb7c2ba962be126ae9c00f121e3562
-        //TRX-HASH  : 9abcbf676f3b374e10d37bf06ef69224ac35676a62734a23f5b790815eb743ba
-        //SIGNATURE : de56cd22f0f006c9cf1b7d96fdbbaa29800be86d57961b55ea42dbe66e0edb953777990c13adcc5de39cf670a46f18148d7290a110da92ea76ac405f23428ff41b
-        println!("Entering into interchaintrx");
-        let address = "8be74d722deb7c2ba962be126ae9c00f121e3562";
-        let message = "9abcbf676f3b374e10d37bf06ef69224ac35676a62734a23f5b790815eb743ba";
-        let signature = "de56cd22f0f006c9cf1b7d96fdbbaa29800be86d57961b55ea42dbe66e0edb953777990c13adcc5de39cf670a46f18148d7290a110da92ea76ac405f23428ff41b";
+        // Result: false (Address is Wrong)
+        let address = "8ce74d722deb7c2ba962be126ae9c00f121e3562";
+        let message = "e62db8a544dfc640c63703f53b91579f7fec46f066432b56ec7daf5a0b7740bc";
+        let signature = "c7acecc3e84328c32fd6ee8a5264e203d371a1bff5561176906afdcf988a32097504b088abac55575820fc1db637b138bb05de61c942847809a1ee127866dba31c";
         let together = format!("{}{}{}", address, message, signature);
-        println!("Final Input String : {:?}", together);
         let input1 = read_hex(&together).unwrap();
         let ret = inter_chain_trx(&input1);
-        //assert_eq!(ret.err(), Some(SecpError::IncorrectSignature));
-        assert_eq!(ret.err(), None);
-        //assert_eq!(ret.ok(), Some(false));
+        assert_eq!(ret.ok(), Some(false));
+    }
+
+    #[test]
+    fn test_inter_chain_trx_3() {
+        // Result: false (Transaction Hash is Wrong)
+        let address = "8be74d722deb7c2ba962be126ae9c00f121e3562";
+        let message = "e63db8a544dfc640c63703f53b91579f7fec46f066432b56ec7daf5a0b7740bc";
+        let signature = "c7acecc3e84328c32fd6ee8a5264e203d371a1bff5561176906afdcf988a32097504b088abac55575820fc1db637b138bb05de61c942847809a1ee127866dba31c";
+        let together = format!("{}{}{}", address, message, signature);
+        let input1 = read_hex(&together).unwrap();
+        let ret = inter_chain_trx(&input1);
+        assert_eq!(ret.ok(), Some(false));
+    }
+
+    #[test]
+    fn test_inter_chain_trx_4() {
+        // Result: false (Transaction Hash Signature is Wrong)
+        let address = "8be74d722deb7c2ba962be126ae9c00f121e3562";
+        let message = "e62db8a544dfc640c63703f53b91579f7fec46f066432b56ec7daf5a0b7740bc";
+        let signature = "d7acecc3e84328c32fd6ee8a5264e203d371a1bff5561176906afdcf988a32097504b088abac55575820fc1db637b138bb05de61c942847809a1ee127866dba31c";
+        let together = format!("{}{}{}", address, message, signature);
+        let input1 = read_hex(&together).unwrap();
+        let ret = inter_chain_trx(&input1);
+        assert_eq!(ret.ok(), Some(false));
+    }
+
+    #[test]
+    fn test_inter_chain_trx_5() {
+        // Result: false (Failed Transaction - Address, Transaction Hash)
+        let address = "b4066e3f167a4e5b7b97cca2b163ee396cfd0a1d";
+        let message = "a0a5e34b9b19b398c5a073513ecb461899ceb45246f51e6d470ae0cf23b39075";
+        let signature = "d7acecc3e84328c32fd6ee8a5264e203d371a1bff5561176906afdcf988a32097504b088abac55575820fc1db637b138bb05de61c942847809a1ee127866dba31c";
+        let together = format!("{}{}{}", address, message, signature);
+        let input1 = read_hex(&together).unwrap();
+        let ret = inter_chain_trx(&input1);
+        assert_eq!(ret.ok(), Some(false));
     }
 }
